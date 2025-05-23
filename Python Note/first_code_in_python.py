b8e023df3aa42f427479eba853e31e83a7010098
Yaxisus <141726205+Axi-Yeasin@users.noreply.github.com>
2025-05-24 03:47:40 +0600

    Add files via upload

---
+++ Python Note/first_code_in_python.py
@@ -0,0 +1,11 @@
+# Hello this is the first comment at this note of python!
+
+var = "Yaxisus!"
+print(var)
+
+
+#Let's try Escape sequence!
+print("Hello future self\nHow are you?\nAre you learning codes?") #That's great. It's working!
+
+#Let's try one more Escape sequence!
+print("Don't waste \"time\", learn code!")
---
+++ Python Note/variables.py
@@ -0,0 +1,48 @@
+# Hey lazy person, Welcome back & don't laugh at my english!
+# In python, there's have various types of variables. Let's see what kind of variable python have.
+
+# -> String
+# It's very simple dude! Just write your values in the "" or ''. Let's take a look
+
+str_var = "Hey man! It's string, do you know me"
+print(str_var) # That's good! It's working.
+
+# -> Numeric data: int, float, complex
+
+# -> int
+# -> It's simple as string is. We don't have to do anything, just write a number like 1 or 100 or maybe 1000 or the number you want.
+int_var = 2005
+print(int_var) # Good it's working. What, want to do some addition? Okay Let's do it.
+
+int_var2 = 10 # Let's add 2005 + 10
+print(int_var + int_var2) # It's pretty easy right!
+
+# -> float
+# float is also number, just we have to add dot (.) like 10.5
+float_var = 10.5
+print(float_var) # Let's check it's data type. What, don't know how to check data type? It's easy man, just type "type(var_name)" this function.
+print(type(float_var)) # Awesome!
+
+# -> complex
+# I don't know how complex number works. For declearing complex number just type any complex number like 3j.
+
+complex_var = 3j
+print(complex_var, type(complex_var)) # Yeah that's complex number!
+
+# -> list
+# We declear list in squir brackets. In this brackets we can write multiple values & this datatypes are can be differen. We can separate the values by comma (,) also we can change list value. Let's have a look!
+
+list_var = ["Drawing", "Sketching", "Painting"]
+print(list_var, type(list_var)) # Amazing!
+
+# -> touple
+# Touple is same as list. Just we declear touple by paranthesis brackets & also we can't change touple values.
+
+touple_var = ("Python", "JavaScript", "C")
+print(touple_var, type(touple_var)) # Beautiful
+
+# -> dictionary
+#  dictionary is an unordered collection of data containing a key:value pair. we can declear dictionary by curly brackets.
+
+dictionary_var = {"name": "yaxisus!", "age": 19,}
+print(dictionary_var, type(dictionary_var))
